a9ba05969f16184be3cf611fbe3134badb648722
khanhuseynov <khanhuseynov1307@gmail.com>
2022-12-19 15:10:57 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+function bir(){
+  var bir =  document.getElementById("bir")
+  bir.play()
+}
+function iki(){
+    var iki =  document.getElementById("iki")
+    iki.play()
+}
+function üç(){
+    var üç =  document.getElementById("üç")
+    üç.play()
+}
+function dörd(){
+    var dörd =  document.getElementById("dörd")
+    dörd.play()
+}
+function beş(){
+    var beş =  document.getElementById("beş")
+    beş.play()
+}
+function altı(){
+    var altı =  document.getElementById("altı")
+    altı.play()
+}
+function yeddi(){
+    var yeddi =  document.getElementById("yeddi")
+    yeddi.play()
+}
+function səkkiz(){
+    var səkkiz =  document.getElementById("səkkiz")
+    səkkiz.play()
+}
+function doqquz(){
+    var doqquz =  document.getElementById("doqquz")
+    doqquz.play()
+}
+function on_(){
+    var on =  document.getElementById("on_")
+    on.play()
+}
+function onbir(){
+    var onbir =  document.getElementById("onbir")
+    onbir.play()
+}
+function oniki(){
+    var oniki =  document.getElementById("oniki")
+    oniki.play()
+}
+function onüç(){
+    var onüç =  document.getElementById("onüç")
+    onüç.play()
+}
+function ondörd(){
+    var ondörd =  document.getElementById("ondörd")
+    ondörd.play()
+}
+function onbeş(){
+    var onbeş =  document.getElementById("onbeş")
+    onbeş.play()
+}
+function onaltı(){
+    var onaltı =  document.getElementById("onaltı")
+    onaltı.play()
+}
+function onyeddi(){
+    var onyeddi =  document.getElementById("onyeddi")
+    onyeddi.play()
+}
